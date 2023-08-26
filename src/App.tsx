e23354bd62edc4d9bd463320fecb416c8fa54ea7
sishi <9987574+shen-qun-deng@user.noreply.gitee.com>
2023-08-26 11:54:17 +0800

    router配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { useRoutes} from 'react-router-dom'
+import routes from './router'
 
 function App() {
-  return <div className="App">123</div>
+  return (
+    <div className="App">
+      {useRoutes(routes)}
+    </div>
+  )
 }
 
 export default App
