763948d2b39ff7600faf4263c81d729d0145628b
sishi <9987574+shen-qun-deng@user.noreply.gitee.com>
2023-08-26 17:26:59 +0800

    配置store

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React, { Suspense } from 'react'
 import { useRoutes, Link } from 'react-router-dom'
 import routes from './router'
+import {shallowEqualApp, useAppDispatch, useAppSelector} from './store'
+import {changeMessageAction} from "@/store/modules/counter";
+
 
 function App() {
+  const { count,message } = useAppSelector(
+    (state) => ({
+      count: state.counter.count,
+      message:state.counter.message
+    }),
+    shallowEqualApp
+  )
+
+  const dispatch=useAppDispatch()
+  function handleChangeMessage(msg:string) {
+    dispatch(changeMessageAction(msg))
+  }
+
   return (
     <div className="App">
       <div className="nav">
@@ -11,6 +27,9 @@ function App() {
         <Link to="/focus">关注</Link>
         <Link to="/download">下载客户端</Link>
       </div>
+      <h2>当前击数：{count}</h2>
+      <h2>{message}</h2>
+      <button onClick={()=>handleChangeMessage("hhhh")}>改变message</button>
       <Suspense fallback={'loading...'}>
         <div className="main">{useRoutes(routes)}</div>
       </Suspense>
